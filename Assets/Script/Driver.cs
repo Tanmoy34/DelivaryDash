16a965954be64bfa5c671ee70d3c45b633d1ab2b
Tanmoy Ghatak <111370623+Tanmoy34@users.noreply.github.com>
2026-06-18 19:36:38 +0530

    Movement and Collison Done

--- Assets/Script/Driver.cs
+++ Assets/Script/Driver.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+
+
+    [SerializeField] float steerSeed = 0.5f;
+    [SerializeField] float moveSpeed = 1f;
+
 
     // Update is called once per frame
     void Update()
     {
+
         
+        float move = 0f;
+        float steer = 0f;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;   
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        transform.Translate(0, moveSpeed * move * Time.deltaTime,0);
+        transform.Rotate(0,0,steer * steerSeed * Time.deltaTime);
+
     }
+
+    
 }
